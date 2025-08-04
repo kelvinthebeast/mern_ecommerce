7f87aab06ef0fbe8e56b528f321aef9375193c81
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-08-04 19:43:07 +0700

    api user login

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 
 const controller = require("../controllers/user")
 
-
+router.post('/login', controller.login)
 router.post('/register', controller.register)
+
 module.exports = router
