24568372e02e64134b07c08553cde6ab637eda77
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-08-06 22:09:56 +0700

    add verifyAccessToken

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 
 const controller = require("../controllers/user")
-
+const { verifyAccessToken } = require('../middlewares/verifyToken')
 router.post('/login', controller.login)
 router.post('/register', controller.register)
-
+router.get("/current", verifyAccessToken, controller.getCurrent)
 module.exports = router
