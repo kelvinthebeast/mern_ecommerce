ceb97d7b3f6ff1680e6e77ea97b216c16d4418ef
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-08-06 22:40:15 +0700

    add verifyAccessToken2

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -23,6 +23,27 @@ const verifyAccessToken = asyncHandler( async (req, res, next) => {
     }
 } )
 
+const verifyAccessToken2 = asyncHandler( async (req, res, next) => {
+    if (req?.headers?.authorization?.startsWith("Bearer")) {
+        // xác thưc
+        const token = req.headers.authorization.split(" ")[1]
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodeInfo) => {
+            if (err) return res.status(401).json({
+                success: false,
+                mess: "Invalid access token"
+            })
+            console.log(decodeInfo);
+            req.user = decodeInfo
+            next()
+        })
+    } else {
+        return res.status(401).json({
+            success: false,
+            mess: "Require authentication!!!"
+        })
+    }
+})
 module.exports = {
-    verifyAccessToken
+    verifyAccessToken,
+    verifyAccessToken2
 }
